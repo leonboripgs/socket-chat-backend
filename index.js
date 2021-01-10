97e5fb3a3b5074aac9b3a7131e8be518721744cd
Maxsim <leonboripgs0818@outlook.com>
2021-01-10 05:17:02 -0300

    extend limit

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
-const { generateKeyPairSync } = require("crypto") 
-const fs = require("fs")
+const { generateKeyPairSync } = require("crypto");
+const fs = require("fs");
 
 const fetch = require("node-fetch"); // node only; not needed in browsers
 const { TextEncoder, TextDecoder } = require("util"); // node only; native TextEncoder/Decoder
@@ -16,28 +16,28 @@ const AccountRouter = require("./modules/account/router");
 const MessageRouter = require("./modules/dms/router");
 
 const { publicKey, privateKey } = generateKeyPairSync("rsa", {
-	// The standard secure default length for RSA keys is 2048 bits
+  // The standard secure default length for RSA keys is 2048 bits
   modulusLength: 2048,
   publicKeyEncoding: {
-    type: 'pkcs1',
-    format: 'pem'
-  }, 
-  privateKeyEncoding: { 
-    type: 'pkcs1', 
-    format: 'pem', 
-    cipher: 'aes-256-cbc', 
-    passphrase: ''
-  } 
+    type: "pkcs1",
+    format: "pem",
+  },
+  privateKeyEncoding: {
+    type: "pkcs1",
+    format: "pem",
+    cipher: "aes-256-cbc",
+    passphrase: "",
+  },
 });
 
-fs.writeFile('pbKey', publicKey, 'utf8', function (err) {
-  if (err) return console.log("Error saving publicKey", err)
-  console.log('Public Key created')
-})
-fs.writeFile('pvKey', privateKey, 'utf8', function (err) {
-  if (err) return console.log("Error saving privateKey", err)
-  console.log('Private Key created')
-})
+fs.writeFile("pbKey", publicKey, "utf8", function (err) {
+  if (err) return console.log("Error saving publicKey", err);
+  console.log("Public Key created");
+});
+fs.writeFile("pvKey", privateKey, "utf8", function (err) {
+  if (err) return console.log("Error saving privateKey", err);
+  console.log("Private Key created");
+});
 
 let app = express();
 
@@ -45,11 +45,13 @@ let app = express();
 app.use(express.static("uploads"));
 app.use(cors());
 app.use(morgan("combined"));
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json());
 
-app.use('/api/account', AccountRouter);
-app.use('/api/message', MessageRouter);
+app.use("/api/account", AccountRouter);
+app.use("/api/message", MessageRouter);
 
 app.get("/", function (req, res) {
   return res.send("Hello new World");
@@ -75,24 +77,24 @@ io.getSocket().on("connection", function (socket) {
     console.log(data);
     onlineUsers[data] = {
       socket: socket,
-      state: true
-    }
-  })
+      state: true,
+    };
+  });
 
   socket.on("disconnect-user", function (data) {
     onlineUsers[data] = {
       socket: null,
-      state: false
-    }
-  })
+      state: false,
+    };
+  });
 
-  socket.on('send:message', function (data) {
-    if ((data.to in onlineUsers) && onlineUsers[data.to].socket) {
-      console.log(data)
-      onlineUsers[data.to].socket.emit("receive:message", data)
-      onlineUsers[data.to].socket.emit("receive:message-contact", data)
+  socket.on("send:message", function (data) {
+    if (data.to in onlineUsers && onlineUsers[data.to].socket) {
+      console.log(data);
+      onlineUsers[data.to].socket.emit("receive:message", data);
+      onlineUsers[data.to].socket.emit("receive:message-contact", data);
     } else {
-      console.log("can not find socket user")
+      console.log("can not find socket user");
     }
-  })
+  });
 });
